8a3acb454fc7fc00354eb861b5f8d081a9a1ef41
chipaaco <169686750+chipaaco@users.noreply.github.com>
2025-11-24 18:46:45 -0300

    4. count words

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def get_book_text():
+    word_number = 0
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    for word in file_contents.split():
+        word_number += 1
+    print(f"Found {word_number} total words")
 
 def main():
     get_book_text()
