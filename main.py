5cc84e042444982a15b4f68bcca7b74014869941
chipaaco <169686750+chipaaco@users.noreply.github.com>
2025-11-25 03:33:14 -0300

    6 count characters dict

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from stats import get_num_words
+from stats import get_num_times_each_word
 
 def main():
     get_num_words()
+    dict = get_num_times_each_word()
+    print(dict)
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,28 @@ def get_num_words():
     for word in file_contents.split():
         word_number += 1
     print(f"Found {word_number} total words")
+
+def get_num_times_each_word():
+    raw_chars = []
+    dictionary = {}
+
+    # lista con caracteres
+    # limpiado tambien de saltos y caracteres especiales asdfasdf
+    with open("books/frankenstein.txt") as f:
+        for digit in f.read():
+            if digit != '\ufeff' and digit != "\n":
+                raw_chars.append(digit.lower())
+    # print(raw_chars)
+
+    # llenar diccionario
+    # sumar 1 segun cada keyvalue posicionado: word count
+    for char in raw_chars:
+        if char in dictionary:
+            dictionary[char] = dictionary[char]+1
+            # print(f"existe keyvalue: {char} wc: {dictionary[char]}")
+        else:
+            dictionary[char] = 1
+            # print(f"se crea keyvalue: {char} wc: {dictionary[char]}")
+
+    # imprimir la wea
+    return dictionary
